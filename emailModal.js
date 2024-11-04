180bb1b426686e6748068505539cf482ca0fefcd
SaraYohannes <sara.seacucumber@gmail.com>
2024-11-04 09:06:27 +0100

    test
    is js changing css?

--- emailModal.js
+++ emailModal.js
@@ -1,10 +1,14 @@
 // describes functionality of the email modal
 
 // close modal button
+const element = document.getElementsByTagName("p");
 const modal = document.getElementsByClassName("email-modal");
 const butt_close = document.getElementsByClassName("close-button")[0];
+
+element.innerHTML = "Hellow World!"
+
 butt_close.onclick = function () {
-    modal.setAttribute("display", "none");
+    modal.style.position = "inline";
 }
 
 // open modal button
