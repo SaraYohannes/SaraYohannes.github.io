b5e6e1fa7014d86bb1a61569d5a669e47e141b96
SaraYohannes <sara.seacucumber@gmail.com>
2024-11-04 09:47:27 +0100

    added butt_open to emailmodal.js
    opens up modal!

--- emailModal.js
+++ emailModal.js
@@ -3,7 +3,11 @@
 // close modal button
 const modal = document.getElementsByClassName("email-modal")[0];
 const butt_close = document.getElementsByClassName("close-button")[0];
-const butt_open = document.get
+const butt_open = document.getElementsByClassName("open-button")[0]
+
+butt_open.onclick = function () {
+    modal.style.display = "block";
+}
 butt_close.onclick = function () {
     modal.style.display = "none";
 }
