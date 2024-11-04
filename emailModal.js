fc6032bce25d1738592ebfa799ca3893d7c56323
SaraYohannes <sara.seacucumber@gmail.com>
2024-11-04 08:35:38 +0100

    update emailmodal.js, close button func

--- emailModal.js
+++ emailModal.js
@@ -1,7 +1,11 @@
 // describes functionality of the email modal
 
 // close modal button
-
+const modal = document.getElementById("email-modal");
+const butt_close = document.getElementsByClassName("close-Button")[0];
+butt_close.onclick = function () {
+    modal.style.display = "none";
+}
 
 // open modal button
 // send mail button?
