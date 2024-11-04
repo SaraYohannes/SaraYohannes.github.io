b98fb9acabad3c637272cba585abecef8df46e9d
SaraYohannes <sara.seacucumber@gmail.com>
2024-11-04 09:36:46 +0100

    added an a href to HTML
    email-link?

--- emailModal.js
+++ emailModal.js
@@ -3,7 +3,7 @@
 // close modal button
 const modal = document.getElementsByClassName("email-modal")[0];
 const butt_close = document.getElementsByClassName("close-button")[0];
-
+const butt_open = document.get
 butt_close.onclick = function () {
     modal.style.display = "none";
 }
