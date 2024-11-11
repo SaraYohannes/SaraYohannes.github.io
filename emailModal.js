ba1b1cbd42dd5539b1581062e2d25dc5000a6f05
SaraYohannes <sara.seacucumber@gmail.com>
2024-11-11 09:47:28 +0100

    added nor and eng translation of all the text
    added two functions changes all innerHTML to either NOR or ENG
    added const ref to id
    added id to all HTML elements with text component

--- emailModal.js
+++ emailModal.js
@@ -3,6 +3,75 @@
 const modal = document.getElementsByClassName("email_modal")[0];
 const butt_close = document.getElementsByClassName("close_button")[0];
 const butt_open = document.getElementsByClassName("open_button")[0];
+const lang_no = document.getElementById("lang-no");
+const lang_en = document.getElementById("lang-en");
+
+
+lang_en.onclick = function () {
+    /* meta */
+    document.getElementsById("page-title").innerHTML = "Sara Yohanne's resume";
+    /* e-mail modal */
+    document.getElementById("emodal-info").innerHTML = "My contact information. Please click to copy text.";
+    document.getElementById("c1").innerHTML = "Phone number";
+    document.getElementById("c2").innerHTML = "E-mail";
+    document.getElementById("c3").innerHTML = "Home address";
+    document.getElementById("c4").innerHTML = "All contact information";
+    /* contact information column */
+    document.getElementById("info-col-nr").innerHTML = "My phone number";
+    document.getElementById("info-col-sendMe").innerHTML = "Send me a mail";
+    document.getElementById("info-col-openModal").innerHTML = "Copy my contact info";
+    /* about me information column */
+    document.getElementById("info-col-aboutMe").innerHTML = "about me";
+    document.getElementById("info-col-am-text").innerHTML = "information";
+    document.getElementById("info-col-relEduc").innerHTML = "relevant education";
+    document.getElementById("info-col-re-text").innerHTML = "information";
+      /* button links */
+    document.getElementById("portf_b_git").innerHTML = "My github account";
+    document.getElementById("portf_b_sketch").innerHTML = "My Sketchfab account";
+    document.getElementById("portf_b_devi").innerHTML = "My Deviantart account";
+    /* project information column */
+    document.getElementById("info-col-projectInfo").innerHTML = "Project information";
+      /* Github pages project */
+    document.getElementById("Project-githubPages").innerHTML = "Homepage";
+    document.getElementById("Project-githubPages-description").innerHTML =
+        "Used this project to learn about HTML, CSS, and JavaScript. The result was a homepage which also serve as my resume";
+      /* Project two */
+    /*document.getElementById("info-col-projectInfo").innerHTML = "Copy my phone contact info";*/
+      /* Project three */
+    /*document.getElementById("info-col-projectInfo").innerHTML = "Copy my phone contact info";*/
+    /* footer */
+    /*document.getElementById("info-col-openModal").innerHTML = "Copy my phone contact info";*/
+
+}
+lang_no.onclick = function () {
+    /* meta */
+    document.getElementsById("page-title").innerHTML = "Sara Yohanne's CV";
+    /* e-mail modal */
+    document.getElementById("emodal-info").innerHTML = "Kontaktinformasjon. Klikk for å kopiere.";
+    document.getElementById("c1").innerHTML = "Mobil nummer";
+    document.getElementById("c2").innerHTML = "E-post";
+    document.getElementById("c3").innerHTML = "Hjemmeadresse";
+    document.getElementById("c4").innerHTML = "All kontaktinformasjonen min";
+    /* contact information column */
+    document.getElementById("info-col-nr").innerHTML = "Mitt nummer";
+    document.getElementById("info-col-sendMe").innerHTML = "Send meg en e-post";
+    document.getElementById("info-col-openModal").innerHTML = "Kopier kontaktinformasjonen min";
+    /* about me information column */
+    document.getElementById("info-col-aboutMe").innerHTML = "om meg";
+    document.getElementById("info-col-am-text").innerHTML = "tekst";
+    document.getElementById("info-col-relEduc").innerHTML = "relevant utdanning";
+    document.getElementById("info-col-re-text").innerHTML = "tekst";
+    /* button links */
+    document.getElementById("portf_b_git").innerHTML = "Min github bruker";
+    document.getElementById("portf_b_sketch").innerHTML = "Min Sketchfab bruker";
+    document.getElementById("portf_b_devi").innerHTML = "Min Deviantart bruker";
+    /* project information column */
+    document.getElementById("info-col-projectInfo").innerHTML = "Prosjekt informasjon";
+    /* Github pages project */
+    document.getElementById("Project-githubPages").innerHTML = "Hjemmeside";
+    document.getElementById("Project-githubPages-description").innerHTML =
+    "Brukte prosjektet til å lære om HTML, CSS og JavaScript. Resultatet ble en hjemmeside som jeg også bruker som CVen min";
+}
 
 // open modal button
 butt_open.onclick = function () {
